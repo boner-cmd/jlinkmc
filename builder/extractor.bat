eb441ea65fb3ddd52f3754db8dc6750025c03f2b
Dimelo Waterson <54084132+boner-cmd@users.noreply.github.com>
2020-04-24 14:12:55 -0500

    jdk definition needs to go in jlink build, not builder build

--- builder/extractor.bat
+++ builder/extractor.bat
@@ -1,8 +1,4 @@
-@ECHO OFF
-SETLOCAL
-SET /p jdk=Enter JDK to use in builder
-@ECHO ON
-docker build --build-arg JDK_VERSION=%jdk% -t bone4cmdr/jlinkmc:builder .
+docker build -t bone4cmdr/jlinkmc:builder .
 docker run --name buildercon bone4cmdr/jlinkmc:builder
 docker cp buildercon:/working/template .\output\Dockerfile
 @ECHO OFF
