795d25b43f7fbd116557eca46eb5f1d6ade5b62e
Dimelo Waterson <54084132+boner-cmd@users.noreply.github.com>
2020-04-24 07:48:38 -0500

    don't echo cleanup ops

--- builder/extractor.bat
+++ builder/extractor.bat
@@ -1,6 +1,7 @@
 docker build -t bone4cmdr/jlinkmc:builder .
 docker run --name buildercon bone4cmdr/jlinkmc:builder
 docker cp buildercon:/working/template .\output\Dockerfile
+@ECHO OFF
 docker stop buildercon >nul
 docker rm buildercon >nul
 PAUSE
