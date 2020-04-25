6fdeaf0412f56b606c11b4ba1601e468f826da14
Dimelo Waterson <54084132+boner-cmd@users.noreply.github.com>
2020-04-25 13:53:02 -0500

    Moved extraction script up one level and added pushd/popd to nav dirs

--- builder/extractor.bat
+++
@@ -1,7 +0,0 @@
-docker build -t bone4cmdr/jlinkmc:builder .
-docker run --name buildercon bone4cmdr/jlinkmc:builder
-docker cp buildercon:/working/template .\output\Dockerfile
-@ECHO OFF
-docker stop buildercon >nul
-docker rm buildercon >nul
-PAUSE
---
+++ extractor.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+pushd "%~dp0"
+pushd \builder
+If %errorlevel% NEQ 0 goto:eof
+@ECHO ON
+docker build -t bone4cmdr/jlinkmc:builder .
+docker run --name buildercon bone4cmdr/jlinkmc:builder
+docker cp buildercon:/working/template .\output\Dockerfile
+@ECHO OFF
+docker stop buildercon >nul
+docker rm buildercon >nul
+@ECHO ON
+popd
+cp \builder\output\Dockerfile .
+@ECHO OFF
+setlocal
+set /p jdkvers_=Use which JDK version? :
+@ECHO ON
+docker build --build-arg JDK_VERSION=%jdkvers_% -t bone4cmdr/jlinkmc:%jdkvers_% .
+PAUSE
