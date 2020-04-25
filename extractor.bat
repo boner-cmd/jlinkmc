9bb4a22fb7295428827d9f54556f073f5cf1ecf9
Dimelo Waterson <54084132+boner-cmd@users.noreply.github.com>
2020-04-25 17:33:45 -0500

    moved tag to the org repo in case it helps with future autobuilding

--- extractor.bat
+++ extractor.bat
@@ -18,5 +18,6 @@ setlocal
 set jdk_version_=14
 set /p jdk_version_=Use which JDK version? :
 @ECHO ON
-docker build --build-arg JDK_VERSION=%jdk_version_% -t bone4cmdr/jlinkmc:jdk%jdk_version_% .
+docker build --build-arg JDK_VERSION=%jdk_version_% -t ethco/jlinkmc:jdk%jdk_version_% .
+docker push ethco/jlinkmc:jdk%jdk_version_%
 PAUSE
