f435224d26955097d5eb223fafb6da2bdde7c010
Dimelo Waterson <54084132+boner-cmd@users.noreply.github.com>
2020-11-16 00:44:58 -0600

    cut unused function, removed unneeded pushd/popd

--- sandbox/builder.sh
+++ sandbox/builder.sh
@@ -1,73 +1,66 @@
 #!/bin/bash
-# needs bash for pushd, popd, and array functionality when getting sums
+# needs bash for array functionality when getting sums
 
-main() {
-  if [ ! -e template ]; then
-    echo "No template in current directory." 1>&2 \
-      && exit 1
-  fi
+if [ ! -e template ]; then
+  echo "No template in current directory." 1>&2 \
+    && exit 1
+fi
 
-  readonly GLIBC_BASE_URL="https://github.com/sgerrand/alpine-pkg-glibc/releases"
-  readonly SASHA_GERRAND_KEY_URL="https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub"
+readonly GLIBC_BASE_URL="https://github.com/sgerrand/alpine-pkg-glibc/releases"
+readonly SASHA_GERRAND_KEY_URL="https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub"
 
-  GLIBC_VER="$(curl -sS ${GLIBC_BASE_URL}/latest)" \
-    && GLIBC_VER="${GLIBC_VER#*tag/}" \
-    && GLIBC_VER="${GLIBC_VER%\"*}" \
-    && readonly GLIBC_VER \
-    || exit 1
-
-  readonly GLIBC_FULL_URL="${GLIBC_BASE_URL}/download/${GLIBC_VER}/glibc-${GLIBC_VER}.apk"
-  readonly GLIBC_BIN_URL="${GLIBC_BASE_URL}/download/${GLIBC_VER}/glibc-bin-${GLIBC_VER}.apk"
-  readonly GLIBC_I18N_URL="${GLIBC_BASE_URL}/download/${GLIBC_VER}/glibc-i18n-${GLIBC_VER}.apk"
-  readonly GLIBC_FILENAME="glibc-$GLIBC_VER.apk"
-  readonly GLIBC_BIN_FILENAME="glibc-bin-$GLIBC_VER.apk"
-  readonly GLIBC_I18N_FILENAME="glibc-i18n-$GLIBC_VER.apk"
-
-  if [ ! -d working ]; then
-    mkdir working
-  fi
-
-  pushd working || exit 1
-
-  # file downloads are slow; output not suppressed to show script did not hang
-  curl -Lf "${SASHA_GERRAND_KEY_URL}" -O \
-    || exit 1
-  curl -Lf "${GLIBC_FULL_URL}" "${GLIBC_BIN_URL}" "${GLIBC_I18N_URL}" -O -O -O \
-    || exit 1
-
-  declare -a sums
-  for file in *; do
-    sums+=("$(sha256sum "./${file}" | head -c 64)")
-  done
-  readonly sums
-
-  # indices are hardcoded to assume only these four (4) files in this order:
-  # GLIBC, GLIBC BIN, GLIBC I18N, Sasha Gerrand's RSA
-  readonly GLIBC_FILE_SUM="${sums[0]}"
-  readonly GLIBC_BIN_SUM="${sums[1]}"
-  readonly GLIBC_I18N_SUM="${sums[2]}"
-  readonly SASHA_GERRAND_RSA_SUM="${sums[3]}"
-
-  popd || exit 1
-  cp ./template ./working/template
-  cd working || exit 1
-
-  sed -i "s,SASHA_GERRAND_RSA_SUM_,${SASHA_GERRAND_RSA_SUM},; \
-    s,SASHA_GERRAND_KEY_URL_,${SASHA_GERRAND_KEY_URL},; \
-    s,GLIBC_FULL_URL_,${GLIBC_FULL_URL},; \
-    s,GLIBC_VER_,${GLIBC_VER},; \
-    s,GLIBC_FILENAME_,${GLIBC_FILENAME},; \
-    s,GLIBC_FILE_SUM_,${GLIBC_FILE_SUM},; \
-    s,GLIBC_BIN_URL_,${GLIBC_BIN_URL},; \
-    s,GLIBC_BIN_FILENAME_,${GLIBC_BIN_FILENAME},; \
-    s,GLIBC_BIN_SUM_,${GLIBC_BIN_SUM},; \
-    s,GLIBC_I18N_URL_,${GLIBC_I18N_URL},; \
-    s,GLIBC_I18N_FILENAME_,${GLIBC_I18N_FILENAME},; \
-    s,GLIBC_I18N_SUM_,${GLIBC_I18N_SUM}," template \
+GLIBC_VER="$(curl -sS ${GLIBC_BASE_URL}/latest)" \
+  && GLIBC_VER="${GLIBC_VER#*tag/}" \
+  && GLIBC_VER="${GLIBC_VER%\"*}" \
+  && readonly GLIBC_VER \
   || exit 1
 
-  cat template
-}
+readonly GLIBC_FULL_URL="${GLIBC_BASE_URL}/download/${GLIBC_VER}/glibc-${GLIBC_VER}.apk"
+readonly GLIBC_BIN_URL="${GLIBC_BASE_URL}/download/${GLIBC_VER}/glibc-bin-${GLIBC_VER}.apk"
+readonly GLIBC_I18N_URL="${GLIBC_BASE_URL}/download/${GLIBC_VER}/glibc-i18n-${GLIBC_VER}.apk"
+readonly GLIBC_FILENAME="glibc-$GLIBC_VER.apk"
+readonly GLIBC_BIN_FILENAME="glibc-bin-$GLIBC_VER.apk"
+readonly GLIBC_I18N_FILENAME="glibc-i18n-$GLIBC_VER.apk"
 
-main
-exit 0
+if [ ! -d working ]; then
+  mkdir working
+fi
+
+cd working || exit 1
+
+# file downloads are slow; output not suppressed to show script did not hang
+curl -Lf "${SASHA_GERRAND_KEY_URL}" -O \
+  || exit 1
+curl -Lf "${GLIBC_FULL_URL}" "${GLIBC_BIN_URL}" "${GLIBC_I18N_URL}" -O -O -O \
+  || exit 1
+
+declare -a sums
+for file in *; do
+  sums+=("$(sha256sum "./${file}" | head -c 64)")
+done
+readonly sums
+
+# indices are hardcoded to assume only these four (4) files in this order:
+# GLIBC, GLIBC BIN, GLIBC I18N, Sasha Gerrand's RSA
+readonly GLIBC_FILE_SUM="${sums[0]}"
+readonly GLIBC_BIN_SUM="${sums[1]}"
+readonly GLIBC_I18N_SUM="${sums[2]}"
+readonly SASHA_GERRAND_RSA_SUM="${sums[3]}"
+
+cp ../template template
+
+sed -i "s,SASHA_GERRAND_RSA_SUM_,${SASHA_GERRAND_RSA_SUM},; \
+  s,SASHA_GERRAND_KEY_URL_,${SASHA_GERRAND_KEY_URL},; \
+  s,GLIBC_FULL_URL_,${GLIBC_FULL_URL},; \
+  s,GLIBC_VER_,${GLIBC_VER},; \
+  s,GLIBC_FILENAME_,${GLIBC_FILENAME},; \
+  s,GLIBC_FILE_SUM_,${GLIBC_FILE_SUM},; \
+  s,GLIBC_BIN_URL_,${GLIBC_BIN_URL},; \
+  s,GLIBC_BIN_FILENAME_,${GLIBC_BIN_FILENAME},; \
+  s,GLIBC_BIN_SUM_,${GLIBC_BIN_SUM},; \
+  s,GLIBC_I18N_URL_,${GLIBC_I18N_URL},; \
+  s,GLIBC_I18N_FILENAME_,${GLIBC_I18N_FILENAME},; \
+  s,GLIBC_I18N_SUM_,${GLIBC_I18N_SUM}," template \
+|| exit 1
+
+cat template
